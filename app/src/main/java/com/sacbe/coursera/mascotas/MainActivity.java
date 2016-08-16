3dd42d54d078fbe5d7d570b72c38ba83dc273eb2
Axel Segura <axelsfla@hotmail.com>
2016-08-16 14:02:29 -0500

    aaa
    aaa

---
+++ app/src/main/java/com/sacbe/coursera/mascotas/MainActivity.java
@@ -0,0 +1,67 @@
+package com.sacbe.coursera.mascotas;
+
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    SwipeRefreshLayout sfiMiIndicadorRefresh;
+    ListView lstMiLista;
+    Adapter adaptador;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        agregarFAB();
+
+        sfiMiIndicadorRefresh = (SwipeRefreshLayout) findViewById(R.id.sfiMiIndicadorRefresh);
+        lstMiLista = (ListView) findViewById(R.id.lstMiLista);
+        String[] planetas = getResources().getStringArray(R.array.planetas);
+        lstMiLista.setAdapter(new ArrayAdapter(this,android.R.layout.simple_list_item_1,planetas));
+        sfiMiIndicadorRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refrescandoContenido();
+            }
+        });
+    }
+
+    public void refrescandoContenido(){
+
+        String[] planetas = getResources().getStringArray(R.array.planetas);
+
+        lstMiLista.setAdapter(new ArrayAdapter(this,android.R.layout.simple_list_item_1,planetas));
+        sfiMiIndicadorRefresh.setRefreshing(false);
+
+    }
+
+    public void agregarFAB(){
+
+        FloatingActionButton miFAB = (FloatingActionButton) findViewById(R.id.fabMiFAB);
+        miFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                //Toast.makeText(getBaseContext(),getResources().getString(R.string.mensaje_toas1),Toast.LENGTH_SHORT).show();
+                Snackbar.make(v,getResources().getString(R.string.mensaje_toas1), Snackbar.LENGTH_SHORT)
+                        .setAction(getResources().getString(R.string.texto_accion), new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Log.i("SNACKBAR","Click en Snackbar");
+                            }
+                        })
+                        .setActionTextColor(getResources().getColor(R.color.colorOroPuma))
+                        .show();
+            }
+        });
+    }
+}
